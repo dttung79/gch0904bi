2647de63b4c6ece9b0b56b4f755a5040e9177274
dttung79 <dttung@gmail.com>
2022-06-01 09:54:38 +0700

    day 6

--- vending.py
+++ vending.py
@@ -6,22 +6,30 @@ COFFEE_PRICE = 20000
 LEMONADE_PRICE = 15000
 ADMIN_CODE = 1234
 running = True
-while running:
+
+def print_menu():
     print('Automatic Vending Machine')
     print('1. Pepsi')
     print('2. Coffee')
     print('3. Lemonade')
+
+def pay(choice):
+    payment = PEPSI_PRICE if choice == PEPSI else COFFEE_PRICE if choice == COFFEE else LEMONADE_PRICE
+    print('Price is', payment)
+    invalid_payment = True 
+    while invalid_payment:
+        amount = int(input('Enter payment amount: '))
+        invalid_payment = amount < payment
+        print('Invalid payment') if invalid_payment else print('Change is', amount - payment)
+
+# main script
+while running:
+    print_menu()
     choice = int(input('Enter your choice: '))
     if choice == ADMIN_CODE:
         print('Shutdown ...')
         break
-    if choice != PEPSI and choice != COFFEE and choice != LEMONADE:
+    if choice not in [PEPSI, COFFEE, LEMONADE]:
         print('Invalid choice')
     else:
-        payment = PEPSI_PRICE if choice == PEPSI else COFFEE_PRICE if choice == COFFEE else LEMONADE_PRICE
-        print('Price is', payment)
-        invalid_payment = True 
-        while invalid_payment:
-            amount = int(input('Enter payment amount: '))
-            invalid_payment = amount < payment
-            print('Invalid payment') if invalid_payment else print('Change is', amount - payment)
+        pay(choice)
